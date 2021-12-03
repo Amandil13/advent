98598a3a52960ac07160fa1a5f44e1872723d25f
Scott <scott@DESKTOP-3MPQ0LH.localdomain>
2021-12-02 21:48:26 -0700

    template file for easier starts and pep8 power

---
+++ 2021/template.py
@@ -0,0 +1,21 @@
+#!/usr/bin/boithon
+
+def part_one(the_input):
+    return 
+
+
+def part_two(the_input):
+    return 
+
+
+if __name__ == "__main__":
+    the_input = list()
+    with open('3.sample') as f:
+    #with open('3.in') as f:
+        for line in f:
+            the_input.append(int(line))
+
+    part_one = part_one(the_input)
+    part_two = part_two(the_input)
+    print(f"Part 1: {part_one}")
+    print(f"Part 2: {part_two}")
