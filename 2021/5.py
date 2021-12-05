c0e2b09b332a3abe81f8bf06ac643687634e20c6
Scott <scott@DESKTOP-3MPQ0LH.localdomain>
2021-12-05 11:43:46 -0700

    improved with better data structures

--- 2021/5.py
+++ 2021/5.py
@@ -1,72 +1,63 @@
 #!/usr/bin/boithon
+
+
 import re
-
-def part_one(the_input):
-    return 
+from collections import defaultdict,namedtuple
 
 
-def part_two(the_input):
-    return 
+def part_one(vectors):
+    grid = defaultdict(int)
+    for vector in vectors:  
+        draw_line(grid,vector,False)
+    return score_grid(grid)
 
-def draw_line(grid,start_x,start_y,end_x,end_y,diagonals): 
-    #find the direction of our lines
-    x_step = 0
-    if start_x > end_x: 
-        x_step = -1
-    elif start_x < end_x: 
-        x_step = 1
-    y_step = 0
-    if start_y > end_y: 
-        y_step = -1
-    elif start_y < end_y: 
-        y_step = 1
+def part_two(vectors):
+    grid = defaultdict(int)
+    for vector in vectors:  
+        draw_line(grid,vector,True)
+    return score_grid(grid)
+
+def draw_line(grid,vector,diagonals): 
+    #Negated compare to find the direction of our lines
+    x_step = (vector[0].x < vector[1].x) - (vector[0].x > vector[1].x)
+    y_step = (vector[0].y < vector[1].y) - (vector[0].y > vector[1].y)
 
     #Bail out if diaganols and they are not enabled
     if x_step != 0 and y_step != 0 and diagonals == False:
         return grid
     
     #Find how many steps
-    stepcount = 0;
-    if x_step != 0:
-        stepcount = abs(start_x - end_x)
-    else: 
-        stepcount = abs(start_y - end_y)
+    stepcount = max(abs(vector[0].x - vector[1].x),abs(vector[0].y - vector[1].y))
 
     #draw the line now
-    x = start_x
-    y = start_y
+    x = vector[0].x
+    y = vector[0].y
     for i in range(0,stepcount + 1): 
-        grid[x][y] += 1
+        grid[Point(x,y)] += 1
         y += y_step
         x += x_step
 
     return grid
 
-if __name__ == "__main__":
-    #initialize data
-    grid = list()
-    gridsize = 999 
-    for i in range(0,gridsize):
-        row = list()
-        for j in range(0,gridsize): 
-            row.append(0)
-        grid.append(row)
+def score_grid(grid): 
+    score = 0
+    values = list(grid.values())
+    return len(grid) - values.count(1)
 
-    #loop through the data
+
+if __name__ == "__main__":
+    Point = namedtuple('Point', ['x', 'y'])
+    vectors = list()
+
+    #parse the data
     with open('5.in') as f:
         for line in f:
             #parse the vector
             m= re.search(r"(\d+),(\d+) -> (\d+),(\d+)",line)
-            start_x = int(m.group(1))
-            start_y = int(m.group(2))
-            end_x = int(m.group(3))
-            end_y = int(m.group(4))
-            draw_line(grid,start_x,start_y,end_x,end_y,True)
-        
-    score = 0
-    for i in range(0,gridsize):
-        for j in range(0,gridsize): 
-            if grid[i][j] >= 2:
-                score += 1
+            vector = (Point(int(m.group(1)),int(m.group(2))),Point(int(m.group(3)),int(m.group(4))))
+            vectors.append(vector)
 
-    print(f"Part 1: {score}")
+    part_one = part_one(vectors)
+    print(f"Part 1:\n{part_one}")
+    part_two = part_two(vectors)
+    print(f"Part 2:\n{part_two}")
