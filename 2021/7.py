c4b025663a97ef8a40e7f75c7e2930c8695cadb5
Scott <scott@DESKTOP-3MPQ0LH.localdomain>
2021-12-06 23:57:48 -0700

    fish sex, now crabs?

---
+++ 2021/7.py
@@ -0,0 +1,42 @@
+#!/usr/bin/boithon
+from statistics import median
+
+def calculate_fuel_one(crabs,position): 
+    fuel = 0
+    for crab in crabs: 
+        fuel += int(abs(crab - position))
+    return fuel
+        
+def calculate_fuel_two(crabs,position_wanted): 
+    fuel = 0
+    for crab in crabs: 
+        units_away = int(abs(crab - position_wanted))
+        fuel += int(units_away * units_away / 2 + units_away / 2)
+    return fuel
+
+def search_for_best_fuel(crabs,increment,guess,fuel_function):
+    best_fuel = fuel_function(crabs,guess)
+    test_fuel = fuel_function(crabs,guess + increment) 
+    while test_fuel < best_fuel: 
+        best_fuel = test_fuel
+        guess = guess + increment
+        test_fuel = fuel_function(crabs,guess + increment) 
+    return best_fuel
+
+if __name__ == "__main__":
+    the_input = list()
+    crabs = list() 
+    with open('7.in') as f:
+        for line in f.read().rstrip().split(','):
+            crabs.append(int(line))
+
+    #These are good gueses to start with, median for part 1, average for part 2
+    median = median(crabs)
+    average = round(sum(crabs)/len(crabs),0)
+
+    best_fuel_part_one = min(search_for_best_fuel(crabs,1,median,calculate_fuel_one), search_for_best_fuel(crabs,-1,median,calculate_fuel_one))
+    best_fuel_part_two = min(search_for_best_fuel(crabs,1,median,calculate_fuel_two), search_for_best_fuel(crabs,-1,median,calculate_fuel_two))
+
+    print(f"Part one: {best_fuel_part_one}")
+    print(f"Part one: {best_fuel_part_two}")
+
