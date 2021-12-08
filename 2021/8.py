98b7ef8a72350bbe79fe9d3a4a892c528ff297a0
Scott <scott@DESKTOP-3MPQ0LH.localdomain>
2021-12-08 11:10:48 -0700

    ez

---
+++ 2021/8.py
@@ -0,0 +1,123 @@
+#!/usr/bin/boithon
+
+#Generates statistics on a number that we can use to identify what it is
+def generate_stats(number_display): 
+    #Mappings are numbers within other numbers, for example: to display the number 8, 1 is always displayed within it
+    mappings = list()
+    for wanted_number,wanted_display in number_display.items(): 
+        for number,display in number_display.items(): 
+            found = 1
+            if number == wanted_number:
+                continue
+            for letter in wanted_display:
+                if letter not in display: 
+                    found = 0
+            if found == 1: #We found a mapping
+                mappings.append((wanted_number,number))
+
+    #Lets generate some profiles for every number
+    number_stats = {}
+    for number,display in number_display.items(): 
+        length = len(number_display[number])
+        display = number_display[number]
+        contained_in = list()
+        contains_number = list()
+        for i,j in mappings: 
+            if i == number:
+                contained_in.append(j)
+            if j == number: 
+                contains_number.append(i)
+        
+        number_stats[number] = { 
+            'display': display,
+            'display_length': length,
+            'contained_in': contained_in,
+            'contained_in_length': len(contained_in),
+            'contains_number': contains_number,
+            'contains_number_length': len(contains_number)
+        }
+    return number_stats
+
+#Given the stats of a good display, we can compare a bad display to it and get it working
+def fix_display(good_display_stats,bad_display): 
+    stats = generate_stats(bad_display) 
+    fixed_display = dict()
+    for number,stats in stats.items(): 
+        for good_number,good_stats in good_display_stats.items(): 
+            match = 1
+            for stat in ('display_length','contained_in_length','contains_number_length'): 
+                if stats[stat] != good_stats[stat]:
+                    match = 0
+            if match == 1: 
+                fixed_display[good_number] = stats['display']
+                continue
+    return fixed_display
+
+def part_one(number_stats): 
+    output_values = list()
+    with open('8.in') as f:
+        for line in f:
+            display = dict()
+            display_blob,output_blob = line.split('|')
+            i = 0
+            for d in display_blob.split(): 
+                sorted_d = sorted(d)
+                display[i] = "".join(sorted_d)
+                i += 1
+            display = fix_display(number_stats,display)
+            for output in output_blob.split(): 
+                sorted_output = sorted(output)
+                output = "".join(sorted_output)
+                for item,value in display.items(): 
+                    if output == value: 
+                        output_values.append(item)
+                        continue
+    return output_values.count(1) + output_values.count(4) + output_values.count(7) + output_values.count(8)                
+
+
+def part_two(number_stats): 
+    output_values = list()
+    with open('8.in') as f:
+        for line in f:
+            display = dict()
+            display_blob,output_blob = line.split('|')
+            i = 0
+            for d in display_blob.split(): 
+                sorted_d = sorted(d)
+                display[i] = "".join(sorted_d)
+                i += 1
+            display = fix_display(number_stats,display)
+            number = ""
+            for output in output_blob.split(): 
+                sorted_output = sorted(output)
+                output = "".join(sorted_output)
+                for item,value in display.items(): 
+                    if output == value: 
+                        number += str(item)
+                        continue
+            output_values.append(int(number))
+
+    return sum(output_values)
+
+
+if __name__ == "__main__":
+
+    #Generate statistics on our working display
+    number_display = {
+        1: 'cf',
+        2: 'acdeg',
+        3: 'acdfg',
+        4: 'bcdf',
+        5: 'abdfg',
+        6: 'abdefg',
+        7: 'acf',
+        8: 'abcdefg',
+        9: 'abcdfg',
+        0: 'abcefg'
+    }
+    number_stats = generate_stats(number_display)
+    
+    answer_part_one = part_one(number_stats)
+    print(f"Part one:\n{answer_part_one}") 
+    answer_part_two = part_two(number_stats)
+    print(f"Part two:\n{answer_part_two}") 
